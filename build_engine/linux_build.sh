067227c9ffc06997877e65420fbc3a19c2992573
Eric Seidel <eric@shorebird.dev>
2023-07-25 16:12:18 -0700

    Update targets for linux

--- build_engine/linux_build.sh
+++ build_engine/linux_build.sh
@@ -15,16 +15,20 @@ cd $ENGINE_SRC
 # We could use Linux to generate all of our Android binaries, but we don't yet.
 # https://github.com/flutter/engine/blob/e590b24f3962fda3ec9144dcee3f7565b195839a/ci/builders/linux_android_aot_engine.json#L40
 
+# Linux doesn't seem to use "archive_gen_snapshot" as a target name yet.
+# https://github.com/flutter/flutter/issues/105351#issuecomment-1650686247
+TARGETS="default clang_x64/gen_snapshot flutter/shell/platform/android:embedding_jars flutter/shell/platform/android:abi_jars"
+
 # Android arm64 release
 ./flutter/tools/gn --android --android-cpu=arm64 --runtime-mode=release --no-goma
-ninja -C ./out/android_release_arm64 archive_gen_snapshot
+ninja -C ./out/android_release_arm64 $TARGETS
 
 # Android arm32 release
 ./flutter/tools/gn --runtime-mode=release --android --no-goma
-ninja -C out/android_release archive_gen_snapshot
+ninja -C out/android_release $TARGETS
 
 # Android x64 release
 ./flutter/tools/gn --android --android-cpu=x64 --runtime-mode=release --no-goma
-ninja -C ./out/android_release_x64 archive_gen_snapshot
+ninja -C ./out/android_release_x64 $TARGETS
 
 # We could also build the `patch` tool for Linux here.
