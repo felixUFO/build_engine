331813c21d1981e7ff40591f5ad1162333d5de2c
Eric Seidel <eric@shorebird.dev>
2023-07-25 16:32:17 -0700

    Update linux targets

--- build_engine/linux_upload.sh
+++ build_engine/linux_upload.sh
@@ -22,9 +22,6 @@ HOST_ARCH='linux-x64'
 
 INFRA_ROOT="gs://$STORAGE_BUCKET/flutter_infra_release/flutter/$ENGINE_HASH"
 
-# TODO(eseidel): Hack for eseidel's machine.
-export PATH="$PATH:/c/Users/micro/AppData/Local/cloud-code/installer/google-cloud-sdk/bin"
-
 # Android Arm64 release gen_snapshot
 ARCH_OUT=$ENGINE_OUT/android_release_arm64
 ZIPS_OUT=$ARCH_OUT/zip_archives/android-arm64-release
