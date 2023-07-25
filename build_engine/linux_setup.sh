c3f65e6ddf864b2c136394bb53d603b7070ad4d4
Eric Seidel <eric@shorebird.dev>
2023-07-25 16:39:24 -0700

    Should work

---
+++ build_engine/linux_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+# Usage:
+# ./linux_setup.sh
+
+# Per https://github.com/flutter/flutter/wiki/Setting-up-the-Engine-development-environment
+# Subset of ./flutter/build/install-build-deps-linux-desktop.sh
+sudo apt -y install libfreetype6-dev pkg-config
