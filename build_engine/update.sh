0bf71c6d9a86c174d97b2d895826d86c676c5d3a
Eric Seidel <eric@shorebird.dev>
2023-07-25 14:52:15 -0700

    fix: reorder fetch after `gclient sync`
    Also fixed the new .sh files to be executable.

--- build_engine/update.sh
+++ build_engine/update.sh
@@ -7,6 +7,17 @@
 ENGINE_ROOT=$1
 ENGINE_HASH=$2
 
+# First update our checkouts to the correct versions.
+# gclient sync -r src/flutter@${ENGINE_HASH}
+# doesn't seem to work, it seems to get stuck trying to
+# rebase the engine repo. So we do it manually.
+# Similar to https://bugs.chromium.org/p/chromium/issues/detail?id=584742
+cd $ENGINE_ROOT/src/flutter
+git checkout $ENGINE_HASH
+
+cd $ENGINE_ROOT
+gclient sync
+
 # Update Dart and the engine to make sure we have all the tags from upstream.
 # The version number in the builds seems to depend on these tags?
 cd $ENGINE_ROOT/src/third_party/dart
@@ -16,18 +27,9 @@ fi
 git fetch
 git fetch --tags upstream
 
-# First update our checkouts to the correct versions.
-# gclient sync -r src/flutter@${ENGINE_HASH}
-# doesn't seem to work, it seems to get stuck trying to
-# rebase the engine repo. So we do it manually.
-# Similar to https://bugs.chromium.org/p/chromium/issues/detail?id=584742
 cd $ENGINE_ROOT/src/flutter
 if [[ ! $(git config --get remote.upstream.url) ]]; then
     git remote add upstream https://github.com/flutter/engine.git
 fi
 git fetch
 git fetch --tags upstream
-git checkout $ENGINE_HASH
-
-cd $ENGINE_ROOT
-gclient sync
